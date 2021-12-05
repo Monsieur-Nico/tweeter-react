e5119abe55226b6df93c7cb23ce2eab1ca415bba
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2021-12-04 19:45:18 -0500

    Destructure the HTML and remake using react

---
+++ react-version/src/components/TweetList.js
@@ -0,0 +1,34 @@
+import Tweet from './Tweet.js'
+
+function TweetList() {
+  return (
+    <section className="tweets">
+      <Tweet />
+      <Tweet />
+
+      {/* <article className="tweet">
+        <header className="tweet--header">
+          <img className="tweet--avatar" src="https://i.imgur.com/nlhLi3I.png" alt="..." />
+          <h2 className="tweet--name">Descartes</h2>
+          <small className="tweet--handle">@rd</small>
+        </header>
+
+        <div className="tweet--body">
+          <p>Je pense , donc je suis</p>
+        </div>
+
+        <footer className="tweet--footer">
+          <small className="footer--age">10 days ago<small>
+            <span className="footer--actions">
+              <a href="./#"><i className="fa fa-flag"></i></a>
+              <a href="./#"><i className="fa fa-retweet"></i></a>
+              <a href="./#"><i className="fa fa-heart"></i></a>'
+            </span>
+          </small></small></footer>
+      </article> */}
+
+    </section>
+  );
+}
+
+export default TweetList;
